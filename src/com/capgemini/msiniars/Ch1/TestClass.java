74ead69a07a5f24e8351ab618709f2598444a640
Mateusz Siniarski <mateusz.siniarski@gmail.com>
2017-01-17 14:05:31 +0100

    Partial implementation of the TestClass

--- src/com/capgemini/msiniars/Ch1/TestClass.java
+++ src/com/capgemini/msiniars/Ch1/TestClass.java
@@ -2,6 +2,8 @@ package com.capgemini.msiniars.Ch1;
 
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.time.format.DateTimeFormatter;
 
 /**
  * Created by Mateusz Siniarski on 17.01.2017.
@@ -9,6 +11,7 @@ import java.io.IOException;
 public class TestClass {
 
     public static void main(String[] args) throws IOException {
-
+        Files.createDirectories(Paths.get("logs"));
+        DataTimeFormatter df = DateTimeFormatter.ofPattern("yyyyMMdd_hhmm");
     }
 }
