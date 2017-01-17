5c2ab78b96b404644ee3a6f6ee81f2218906ad1a
Mateusz Siniarski <mateusz.siniarski@gmail.com>
2017-01-17 14:02:23 +0100

    First commit

---
+++ src/com/capgemini/msiniars/Ch1/TestClass.java
@@ -0,0 +1,14 @@
+package com.capgemini.msiniars.Ch1;
+
+
+import java.io.IOException;
+
+/**
+ * Created by Mateusz Siniarski on 17.01.2017.
+ */
+public class TestClass {
+
+    public static void main(String[] args) throws IOException {
+
+    }
+}
---
+++ src/com/capgemini/msiniars/Main.java
@@ -0,0 +1,8 @@
+package com.capgemini.msiniars;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
